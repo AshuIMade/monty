75a2a799125842b438ea1234836562cd4a49743e
AshuIMade <asheimade@gmail.com>
2023-01-18 09:46:45 -0800

    creating functions in pall and push

--- pall.c
+++ pall.c
@@ -1,1 +1,11 @@
 #include "monty.h"
+/**
+ * pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+ */
+void pall(stack_t **stack, unsigned int line_cnt)
+{
+
+}
--- push.c
+++ push.c
@@ -1,1 +1,11 @@
 #include "monty.h"
+/**
+ * push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void push(stack_t **stack, unsigned int line_cnt)
+{
+	
+}
