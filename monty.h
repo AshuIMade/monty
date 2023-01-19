b7031183d4dae5718be8e8c4afd092350d4f3513
AshuIMade <asheimade@gmail.com>
2023-01-19 13:34:46 -0800

    mod, comments and pchar

--- monty.h
+++ monty.h
@@ -14,6 +14,8 @@
 	{"div", _div},    \
 	{"mul", _mul},    \
 	{"sub", _sub},    \
+	{"mod", mod},     \
+	{"pchar", pchar}, \
 	{                     \
 		NULL, NULL      \
 	}                     \
